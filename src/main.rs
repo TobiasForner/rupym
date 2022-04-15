cbab7bdf04284b9c1241d85f08cd3931134d5f2d
Tobias Forner <t.forner@tum.de>
2022-04-15 22:10:41 +0200

    add basic CLI

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
-use std::process::Command;
-
 mod install_dependencies;
 
+use clap::{command, Arg, Command};
 use install_dependencies::install_packages;
 
 fn main() {
-    install_packages(vec!["black", "virtualenv"]);
+    let matches = command!()
+        .propagate_version(true)
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .subcommand(
+            Command::new("add")
+                .about("Adds packages to your project")
+                .arg(Arg::new("packages").required(true).min_values(1)),
+        )
+        .get_matches();
+
+    match matches.subcommand() {
+        Some(("add", packages)) => {
+            let package_names: Vec<&str> = packages.values_of("packages").unwrap().collect();
+            println!("'myapp add' was used, name is: {package_names:?}",);
+            install_packages(package_names);
+        }
+        _ => unreachable!("Exhausted list of subcommands and subcommand_required prevents `None`"),
+    }
 }
